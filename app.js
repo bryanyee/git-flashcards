581529b523f440dc48ff1af4d361d006301e456e
Bryan Yee <yee.bryan.c@gmail.com>
2016-06-05 14:06:44 -0700

    Added random button functionality

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-var 	cardsDictionary = [ {front: "git config --global user.name \"Bryan Yee\"", back: "configures the username"}],
+var 	cardsDictionary = [ {front: "git config --global user.name \"Bryan Yee\"", back: "configures the username"},
+							{front: "git config --global user.email \"yee.bryan.c@gmail.com\"", back: "configures the email address"},
+							{front: "git init projectname", back: "initializes a project repository"} ],
 		dictionaryLength = cardsDictionary.length,
 		flashcard = document.getElementById('flashcard');
 
 
 function randomCard(){
 	var randomIndex = Math.floor(Math.random()*dictionaryLength);
+	if(randomIndex === dictionaryLength) randomIndex--;
+
 	flashcard.innerHTML = cardsDictionary[randomIndex].front;
 }
 
