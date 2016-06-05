81f326679c55759896e3e185a1a4f6e5f1561b9a
Bryan Yee <yee.bryan.c@gmail.com>
2016-06-05 16:18:28 -0700

    Vertically center the flashcard details

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ var 	cardsDictionary = [ {front: "git config --global user.name \"Bryan Yee\"", 
 							{front: "git config --global user.email \"yee.bryan.c@gmail.com\"", back: "configures the email address"},
 							{front: "git init projectname", back: "initializes a project repository"} ],
 		dictionaryLength = cardsDictionary.length,
-		flashcard = document.getElementById('flashcard');
+		flashcardDetail = document.getElementById('flashcardDetail');
 
 
+randomCard();
+
+//Displays a random flashcard
 function randomCard(){
 	var randomIndex = Math.floor(Math.random()*dictionaryLength);
 	if(randomIndex === dictionaryLength) randomIndex--;
 
-	flashcard.innerHTML = cardsDictionary[randomIndex].back;
+	flashcardDetail.innerHTML = cardsDictionary[randomIndex].back;
+	setTopValue();
 }
 
-randomCard();
+//Vertically centers the text in the flashcard
+function setTopValue(){
+	var flashcardDetailHeight = flashcardDetail.clientHeight;
+	flashcardDetail.style.top = ((400 - flashcardDetailHeight) / 2).toString() + "px";
+}
+
